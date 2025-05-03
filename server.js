7571264c8211a731184bc9c1306128061cff0b1a
Prxyankaz <137303413+Prxyankaz@users.noreply.github.com>
2025-05-04 01:54:11 +0530

    port changed too adapt dynamically

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ import {
 import { formatItem, formatItems } from './views/ItemView.js';
 
 const app = express();
-const PORT = 3000;
+
 
 app.use(express.json());
 app.use(express.static('public'));
@@ -41,6 +41,7 @@ app.delete('/items/:id', async (req, res) => {
   success ? res.sendStatus(204) : res.status(404).send('Item not found');
 });
 
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
